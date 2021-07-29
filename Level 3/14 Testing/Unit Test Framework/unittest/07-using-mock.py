9746085ad6da9ccf6667a56c8129e6c7c6a61be8
seddon-software <seddon_software@btinternet.com>
2021-07-29 19:11:38 +0100

    updated gitignore

--- Level 3/14 Testing/Unit Test Framework/unittest/07-using-mock.py
+++ Level 3/14 Testing/Unit Test Framework/unittest/07-using-mock.py
@@ -3,48 +3,47 @@
 # of 'record -> replay' used by many mocking frameworks.
 
 
+import time
 import unittest 
 from unittest.mock import patch
 
-'''This class is called by the class being tested, but we don't want to use the real class
-because it may be unavailable, take too long to run in a unit test or have further dependencies 
-that are not available.  So we mock out the methods used'''
-class MyClass():
+# This class is called by the class being tested, but we don't want to use 
+# the real class because it may be unavailable, take too long to run in a unit
+# test or have further dependencies that are not available.  So we mock out 
+# the methods used:
+class Shares:
+    def __init__(self, name):
+        self.name = name
+
     'This method is mocked out'
-    def my_method(self):
+    def getPrice(self):
         # this method will do something complicated in practice
         # and we don't wan't it to take part in the test
         pass
  
-class ClassUnderTest():
-    'We are testing this method (f) which calls the mocked out method'
-    def f(self):
-        myclass = MyClass()
-        return myclass.my_method()
+# We are testing this class
+class Market:
+    # We are testing the following method, which calls a mocked out method'
+    def calculateChangeInSharePrice(self, share):
+        startingPrice = share.getPrice()    # call mocked out method
+        time.sleep(5)
+        finishingPrice = share.getPrice()   # call mocked out method
+        return finishingPrice - startingPrice
     
-''' This is the unit test'''
-class testPoint(unittest.TestCase):
-    'testing a method mocked out with a constant return value (True)'
-    @patch.object(MyClass, 'my_method')
-    def test_that_f_returns_true_if_called_only_once(self, mocked_method):
-        mocked_method.return_value=True # make the mock return True when called
-        o =  ClassUnderTest()
-        result = o.f()
-        self.assertTrue(result)
-
-    'testing a method mocked out which returns the successive results (False, False, True)'
-    @patch.object(MyClass, 'my_method')
-    def test_that_f_returns_False_False_True_on_successive_calls(self, mocked_method):
-        return_values= [False,False,True]
+# The test(s) follow:
+class testStocks(unittest.TestCase):
+    'testing a method that calls a mocked out method'
+    @patch.object(Shares, 'getPrice')
+    def test_change_in_share_price(self, mocked_getPrice):
+        return_values= [31.2, 25.7]  # values in reverse order
         def side_effect():
             return return_values.pop()
-        mocked_method.side_effect = side_effect # make the mock return False, False, True on successive calls
-
-        o =  ClassUnderTest()
-        self.assertTrue(o.f())
-        self.assertFalse(o.f())
-        self.assertFalse(o.f())
+        mocked_getPrice.side_effect = side_effect
+        ibm =  Shares("IBM")
+        market = Market()
+        change = market.calculateChangeInSharePrice(ibm)
+        self.assertTrue(5.5 == change)
 
 if __name__ == '__main__':
     unittest.main()
-    1
+    
