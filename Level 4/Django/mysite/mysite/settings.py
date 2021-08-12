0f4043cc46f039030aef6b0e8f35847b06869c25
seddon-software <seddon_software@btinternet.com>
2021-08-12 23:05:01 +0100

    added notes for Level 4 EPICS

--- Level 4/Django/mysite/mysite/settings.py
+++ Level 4/Django/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h$q=(#b0jfh76k+xm%(rf+3s_u_@eevv7f1@6pt0#7#ofq=s9$'
+SECRET_KEY = 'django-insecure-(!8mkorj150f!#&!b&$wtw+idpyi&ux!f=vidb++1#+(8el%r$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
