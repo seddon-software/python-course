5bacdda62a43cd87d2d84abb74fee8bb31ef491b
seddon-software <seddon_software@btinternet.com>
2021-08-13 22:07:28 +0100

    minor stuff

--- Level 4/EPICS/caput.py
+++ Level 4/EPICS/caput.py
@@ -8,12 +8,12 @@ print(caget("chris:amplitude"))
 
 for n in np.arange(1.0, 0.1, -0.01):
     caput("chris:amplitude", n)
-    print(caget("chris:amplitude"))
+    print(f'{caget("chris:amplitude"):0.2f}')
     time.sleep(0.25)
 
 for n in np.arange(0.1, 1.0, 0.01):
     caput("chris:amplitude", n)
-    print(caget("chris:amplitude"))
+    print(f'{caget("chris:amplitude"):0.2f}')
     time.sleep(0.25)
 
 pvs = ["chris:function",
