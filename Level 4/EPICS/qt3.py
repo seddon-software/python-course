b4327dce9f98d2723eae0cf1cbc910e387a1202b
seddon-software <seddon_software@btinternet.com>
2021-08-24 17:23:32 +0100

    interim stuff

--- Level 4/EPICS/qt3.py
+++ Level 4/EPICS/qt3.py
@@ -27,12 +27,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
 
-        # plot data: x, y values
-        self.graphWidget.plot(hour, temperature)
+        self.graphWidget.setBackground('w')
 
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
+
+# ... init continued ...
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first
+        self.y.append( randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
