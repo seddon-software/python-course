b41934c78df9e95732e08c05215a923da1eb7dcb
seddon-software <seddon_software@btinternet.com>
2021-08-24 22:23:57 +0100

    interim work

--- Level 4/EPICS/qt3.py
+++ Level 4/EPICS/qt3.py
@@ -4,6 +4,8 @@ import cothread
 from cothread.catools import *
 
 from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
