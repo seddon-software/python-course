a7527b529cff0ec43eea5b0247c6a67f06914bde
seddon-software <seddon_software@btinternet.com>
2021-08-24 21:47:22 +0100

    interim work

--- Level 4/EPICS/qt3.py
+++ Level 4/EPICS/qt3.py
@@ -35,20 +35,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
 # ... init continued ...
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(50)
-        self.timer.timeout.connect(self.update_plot_data2)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
     def update_plot_data(self):
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-
-        self.y = self.y[1:]  # Remove the first
-        self.y.append( randint(0,100))  # Add a new random value.
-
-        self.data_line.setData(self.x, self.y)  # Update the data.
-
-    def update_plot_data2(self):
+        delta = 0.01
         if self.amplitude > 1: delta = -0.01
         if self.amplitude < 0.1: delta = +0.01
         self.amplitude += delta
