fb31241d25636c2a0ee1a86ae6721263adb60002
seddon-software <seddon_software@btinternet.com>
2021-08-27 13:38:38 +0100

    started adding archiver examples

--- Level 4/EPICS/a1.py
+++ Level 4/EPICS/a1.py
@@ -8,15 +8,14 @@ import numpy as np
 event = aa.get_value_at('SR-DI-DCCT-01:SIGNAL', datetime.now())
 print(event.value)
 pv = "SR-DI-DCCT-01:SIGNAL"
-data = aa.get_values(pv, datetime(2021, 1, 5, hour=13, minute=0), datetime(2021, 1, 5, hour=14, minute=0))
-#print(data.values)
+h1 = 0
+h2 = 23
+data = aa.get_values(pv, datetime(2021, 1, 5, hour=h1, minute=0), 
+                         datetime(2021, 1, 5, hour=h2, minute=0))
 data_points = data.values.shape[0]
-X = np.arange(data_points)
+X = np.arange(h1, h2, (h2-h1)/data_points)
 Y = data.values.reshape((data_points,))
-print(X.shape, Y.shape)
 ax = plt.subplot()      # create single figure with one axis
-redCircles = "ro"
 ax.plot(X, Y)
-# ax.plot(X, Y, redCircles)
 plt.show()
 
