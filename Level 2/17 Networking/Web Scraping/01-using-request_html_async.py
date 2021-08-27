cae19be2e283aa106c04cc1fb4d5ba7b9a145a81
seddon-software <seddon_software@btinternet.com>
2021-08-28 00:03:24 +0100

    adding asyncio example to web scraping

--- Level 2/17 Networking/Web Scraping/01-using-request_html_async.py
+++ Level 2/17 Networking/Web Scraping/01-using-request_html_async.py
@@ -1,13 +1,15 @@
-import re
+import re, sys
 import asyncio
 from requests_html import AsyncHTMLSession
+import pyppeteer.launcher as l
+import pyppeteer
 
 # Note: the first time you run this program it will download the Chrome driver
 async def get_quote(symbol):
     url = f'https://www.lse.co.uk/SharePrice.asp?shareprice={symbol}'
     session = AsyncHTMLSession()
     r = await session.get(url)
-    await r.html.arender()
+    await r.html.arender(timeout=50)
     pattern = r'<span.*data-field="MID_PRICE"  data-flash="true">(.*)</span>'
     m = re.search(pattern, r.text)
     if m:
@@ -16,6 +18,7 @@ async def get_quote(symbol):
     else:
         quote = 'no quote available for: ' + symbol
         print(quote)
+
 
 async def main():
     print("London Stock Exchange Quotes")
@@ -31,9 +34,20 @@ async def main():
     await task4
     await task5
     await task6
+    await pyppeteer.launch()
 
-asyncio.run(main())
+def dn(): 
+    sys.exit(0)
 
+
+l.atexit._run_exitfuncs = dn
+
+g = main()
+try:
+    asyncio.run(g)
+    
+except RuntimeError as e:
+    sys.exit(0)
 
 
 
