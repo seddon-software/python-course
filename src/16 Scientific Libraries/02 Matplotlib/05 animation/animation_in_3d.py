4897dadb38ce4b5ec732d0c53f15052da7ce6823
seddon-software <seddon_software@btinternet.com>
2022-03-06 23:07:57 +0000

    more docs

--- src/16 Scientific Libraries/02 Matplotlib/05 animation/animation_in_3d.py
+++ src/16 Scientific Libraries/02 Matplotlib/05 animation/animation_in_3d.py
@@ -1,3 +1,43 @@
+'''
+Animation of a Shape in 3D
+==========================
+
+MatPlotLib supports animation using the function:
+            ani = FuncAnimation(fig, update_plot, frames=frames, fargs=(data,sc), interval=100)
+
+This does not provide sophisticated animation as in games, but is intended to add a timing element to plotting.
+The key part of this call is the second parameter:
+            update_plot
+which specifies a function pointer; the function will be called repeatedly during the animation.  The update will 
+be performed every 100 msec and frames will indicate the number of frame in the animation.  When update_plot
+is called, the frame number will be passed as a monotonically increasing number, starting at 1.  If you look at
+the function def statement:
+            def update_plot(frame, data, sc):
+you will notice two more parameters are passed to update_plot.  These parameters were specified in the "fargs"
+tuple of FuncAnimation.  In this example we calculate all our animation data prior to calling FuncAnimation, 
+but this is not required.
+
+Other points of interest are:
+1)          matplotlib.use('TkAgg')
+This defines the backend graphics driver used by MatPlotLib.  "backend" does all the hard work behind the scenes
+to make draw the figure. There are two types of backends: user interface backends (for use in PyQt/PySide, 
+PyGObject, Tkinter, wxPython, or macOS/Cocoa); also referred to as "interactive backends") and hardcopy backends 
+to make image files (PNG, SVG, PDF, PS; also referred to as "non-interactive backends").  You can list the 
+available backends with:
+            print(matplotlib.rcsetup.interactive_bk)
+            print(matplotlib.rcsetup.non_interactive_bk)
+            print(matplotlib.rcsetup.all_backends)
+Several of the backends will not work with this example.
+
+2) inserting a breakpoint in the code will likely cause a change of backend and the code will fail.  So run this 
+code without breakpoints.
+
+3)  The return from update_frame tells the animation which "artist" to update, in this case our 3D axes (ax).  
+Note this function is very short but does update
+            sc._offsets3d
+which is a poorly documented feature, but should contain the x, y, z coordinates of objects in the scatter plot.
+'''
+
 import numpy as np
 from numpy import array, dot
 import matplotlib
@@ -5,7 +45,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 from matplotlib.animation import FuncAnimation
 from math import sqrt
-
 
 def update_plot(frame, data, sc):
     sc._offsets3d = data[frame]
@@ -40,8 +79,7 @@ def main():
 
     ix, iy, iz = data[0]
     sc = ax.scatter(ix, iy, iz, s=100.0, c='red', marker='d')
-    def init():
-        return ax
+
     ani = FuncAnimation(fig, update_plot, frames=frames, fargs=(data,sc), interval=100)
     plt.show()
 
--- src/16 Scientific Libraries/02 Matplotlib/05 animation/line_plot_in_3d.py
+++ src/16 Scientific Libraries/02 Matplotlib/05 animation/line_plot_in_3d.py
@@ -1,20 +1,45 @@
+'''
+Animation of a Line in 3D
+=========================
+
+This similar to the previous examples, but with a few changes.
+
+1) this time we have an initialization function "init()" which is called to set to scene prior to the first
+frame.  This draws a red circle at the origin.
+
+2) animate produces an array t that has shape (300,) using:
+            t = np.arange(i*dt, i*dt+3, dt)
+The array is used to parameterize the x, y and z arrays. These arrays will also have shape (300,).  That means the
+line will consist of 300 points.  Since t varies on each frame the x, y, z arrays will also change, giving the 
+impression of the line moving.
+
+3) To feed x, y and z into the actual line artist we use:
+            line.set_data(x, y)
+            line.set_3d_properties(z)
+The second line is required to set the z component of the line (because line was originally designed for 2D use 
+and "set_data()" was never updated for the 3D case).
+
+4) the return from "animation.FuncAnimation()" is used solely to keep a reference to the animation object.  
+If you don't store the return, the animation object will get garbage collected and the animation will end
+prematurely.
+'''
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 fig = plt.figure()
-ax = fig.gca(projection="3d")
+fig.canvas.manager.set_window_title("3D Curve Animation")
+ax = fig.add_subplot(111, projection='3d')
 
+# initial view
 elevation = 10
 viewing_angle = 125
-
 ax.view_init(elev=elevation, azim=viewing_angle)
-line, = ax.plot([], [], [], lw=2)
-line2, = ax.plot([], [], [], 'ro')
 
+# set the coordinates for the line as empty and fill in data after animate() called.
+line, = ax.plot([], [], [], lw=2)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
@@ -26,9 +51,7 @@ ax.set_autoscale_on(False)
 dt = 0.01
 
 def init():
-    line2.set_data(0, 0)
-    line2.set_3d_properties(0)
-    return line2
+    ax.plot([0], [0], [0], 'ro')
 
 def animate(i):
     t = np.arange(i*dt, i*dt+3, dt)
@@ -43,17 +66,9 @@ def animate(i):
 # create animation object
 # note anim keeps a reference to the FuncAnimation object
 # without which the animation dies premeturely
-animinmationObject = animation.FuncAnimation(fig, animate, init_func=init, \
-                                             frames=650, interval=50)
+animinmationObject = animation.FuncAnimation(fig, animate, init_func=init, frames=650, interval=50)
 
-plt.gcf().canvas.set_window_title("3D Curve Animation")
 plt.show()
-
-
-
-
-
-
 
 
 
