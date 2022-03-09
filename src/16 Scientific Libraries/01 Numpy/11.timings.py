b261487e6d033d368bc12cf50772fe1398c6f49f
seddon-software <seddon_software@btinternet.com>
2022-03-09 17:22:19 +0000

    minor stuff

--- src/16 Scientific Libraries/01 Numpy/11.timings.py
+++ src/16 Scientific Libraries/01 Numpy/11.timings.py
@@ -1,3 +1,17 @@
+'''
+Timings
+=======
+
+This example compares several different approaches to computing the square roots of the first 10 million integers.
+I've include a C example which needs to be built before you can run this example.  To build the C module, run the 
+scripts in the "C_Library" folder.  If you prefer, you can exclude the C example by setting the boolean:
+            EXCLUDE_C_EXAMPLE=True
+
+Note that Numba runs on parallel CPUs and hence will be very fast; Numpy and C are similiar in speed.
+'''
+
+EXCLUDE_C_EXAMPLE=False
+
 from timeit import timeit
 import numba
 
@@ -29,9 +43,10 @@ def numbaMethod(n):
     y = numpy.sqrt(x)
     return numpy.sum(y)
 
-import _roots as roots
-def cModule(n):
-    return roots.sumOfRoots(n)
+if not EXCLUDE_C_EXAMPLE:
+    import _roots as roots
+    def cModule(n):
+        return roots.sumOfRoots(n)
 
 # set up timers
 from timeit import Timer
@@ -50,7 +65,7 @@ print(forLoop2(n))
 print(listComprehension(n))
 print(numpyMethod(n))
 print(numbaMethod(n))
-print(cModule(n))
+if not EXCLUDE_C_EXAMPLE: print(cModule(n))
 
 # perform timings
 count = 1
@@ -58,7 +73,7 @@ t1 = ForLoop.timeit(number=count)
 t2 = ForLoop2.timeit(number=count)
 t3 = ListComprehension.timeit(number=count)
 t4 = Numpy.timeit(number=count)
-t5 = C_Module.timeit(number=count)
+if not EXCLUDE_C_EXAMPLE: t5 = C_Module.timeit(number=count)
 t6 = Numba.timeit(number=count)
 
 print("{:20s}{:>8s}{:>8s}".format("code", "time", "t2/time"))
@@ -67,7 +82,8 @@ print("{:20s}{:8.3f}{:8.3f}".format("For Looop (v1)"     , t1, t2/t1))
 print("{:20s}{:8.3f}{:8.3f}".format("For Looop (v2):"    , t2, t2/t2))
 print("{:20s}{:8.3f}{:8.3f}".format("List Comprehension:", t3, t2/t3))
 print("{:20s}{:8.3f}{:8.3f}".format("Numpy:"             , t4, t2/t4))
-print("{:20s}{:8.3f}{:8.3f}".format("C Module"           , t5, t2/t5))
+if not EXCLUDE_C_EXAMPLE: 
+    print("{:20s}{:8.3f}{:8.3f}".format("C Module"           , t5, t2/t5))
 print("{:20s}{:8.3f}{:8.3f}".format("Numba:"             , t6, t2/t6))
 
 
