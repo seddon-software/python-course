45e94c699bd75b7c69d2a848bb65cc6de0d262bb
seddon-software <seddon_software@btinternet.com>
2022-04-03 19:23:49 +0100

    more docs

--- src/17 Threading and Concurrency/03 Threading/04-global-interpreter-lock.py
+++ src/17 Threading and Concurrency/03 Threading/04-global-interpreter-lock.py
@@ -1,13 +1,38 @@
-############################################################
-#
-#    global interpreter lock
-#
-############################################################
+'''
+Global Interpreter Lock
+=======================
+The GIL is a lock held by CPython interpreter whenever bytecode is being executed unless it is explicitly released 
+by the programmer.  CPython assumes that whatever occurs between bytecodes is not thread-safe unless told otherwise.
+This implies that the GIL is enabled by default.  CPython releases the GIL periodically to allow other threads to
+run.
+
+Other implementation of the Python interpreter such as Jython and IronPython have no GIL and can fully exploit 
+multithreading.  PyPy currently has a GIL (similar to CPython) and Cython also has a GIL.  Note, the GIL in Cython 
+can be released temporarily using a "with" statement. 
+
+In CPython, the GIL is released every few (<10) msec after completing a byte code instruction.  Operations 
+consisting of a single byte code instruction are atomic and hence thread safe.
+
+A thread may release the GIL voluntarily to allow another thread to run.  A thread only needs to hold the GIL 
+while it works with Python objects, so CPython will release the GIL and allow another thread to run if the thread
+holding the GIL performs I/O operations or other blocking calls into the OS like select() and pthread_mutex_lock().
+The GIL may also be released by library code written in C/C++.  Note that there is no direct way of releasing the 
+GIL in Python code (but there is if you are writing a C/C++ extension).
+
+In the code below we use the dis module to disassemble code for:
+            x += 1
+            sort([2,5,3,6])
+
+Note that the first sequence is mutable and hence not thread safe.  If we are suspended after INPLACE_ADD then
+we would overwrite any work performed by other threads executing the same code.
+
+Surprisingly, although the second sequence is more complicated, it is thread safe.  This is because the byte code
+instructions are all immutable, so the code is idempotent and hence thread safe.  Recall that the GIL will not be 
+released in the middle of a byte code instruction and hence the BUILD_LIST and CALL_FUNCTION (call to sort) 
+instructions are atomic and hence thread safe.
+'''
 
 import dis
-
-# The GIL is released every 15 msec, but not during an atomic operation
-# Operations consisting of a single byte code are thread safe
 
 dis.dis("x += 1")           # not thread safe
 #               0 LOAD_NAME                0 (x)
@@ -21,6 +46,6 @@ dis.dis("sort([2,5,3,6])")  # thread safe
 #               4 LOAD_CONST               1 (5)
 #               6 LOAD_CONST               2 (3)
 #               8 LOAD_CONST               3 (6)
-#              10 BUILD_LIST               4
+#              10 BUILD_LIST               4            <== ATOMIC
 #              12 CALL_FUNCTION            1            <== ATOMIC
 #              14 RETURN_VALUE
--- src/17 Threading and Concurrency/03 Threading/05-sharing-data.py
+++ src/17 Threading and Concurrency/03 Threading/05-sharing-data.py
@@ -1,8 +1,20 @@
-############################################################
-#
-#    sharing data
-#
-############################################################
+'''
+Sharing Data
+============
+As discussed previously, code using += is not thread safe.  As an illustration of this we define two counts and
+then increment these counts in 3 separate threads.  One of the counts is protected by a lock, but the other is not.
+We have to increment many (10 million) times to maximise the chance of being suspended in the critical section of 
+codeen.  The protected count will always end up at 3 threads x 10 million = 30,000,000, but the other count will
+usually end up less as a result of the contention between the threads.
+
+Because programmers are prone to forget to release locks, we give alternate ways of using a lock in threads "B"
+and "C".  Thread "B" uses a finally block and thread "C" uses a with statement.  The with statement is expanded
+by the interpreter to the try-finally form (so these forms are equivalent).
+
+Note that often code is modified after the initial design and we might introduce code that could throw an exception
+between obtaining the lock and releasing it.  That's why it's better to use the with statement than the straight
+lock.release() because with is exception safe.
+'''
 
 from threading import Thread
 from threading import Lock
