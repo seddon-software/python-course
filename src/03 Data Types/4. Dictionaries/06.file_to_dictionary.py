2334be5fd267abd760db640ad05582cb4a6455ad
seddon-software <seddon_software@btinternet.com>
2022-07-19 17:33:57 +0100

    add docs to first part of Level 2

--- src/03 Data Types/4. Dictionaries/06.file_to_dictionary.py
+++ src/03 Data Types/4. Dictionaries/06.file_to_dictionary.py
@@ -4,11 +4,21 @@ Converting a File to a Dictionary
 
 If we have a file containing information of the form of key value pairs, we can read the file into a dictionary.
 Because a dictionary is hashed, we can then retreive information about indivial keys in an efficient manner.
+
+The example file contains international phone codes.  The first field on each line is the phone code which we
+will use as the value in the dictionary.  The rest of the line will be the state, city or region and this will 
+become the key.
+
+I've added a trace to make the code more obvious, but it produces a lot of output.  Switch the trace to False
+to get succint output.
 '''
 
-trace = True
+from pprint import pprint
+trace = False
+# trace = True      # this produces a lot of output
 
 def Print(item):
+    if item is dict and trace: pprint(item)
     if trace: print(item)
 
 # dictionaries can't be sorted, but we can print out
@@ -20,15 +30,13 @@ try:
     f = open("data/codes.txt", "r")
     for line in f:
         Print(f"current line: {line}")
-        strippedLine = line.rstrip().split(' ')
+        strippedLine = line.rstrip().split(' ')  # split string on spaces
         Print(f"current line as list (stripped): {strippedLine}")
-        value = theList[0]
-        x = theList[1:]
-        Print(x)
-        y = "".join(x)
-        Print(y)
-        key = " ".join(theList[1:])
+        value = strippedLine[0]  # this is the area code
+        key = " ".join(strippedLine[1:]) # this is the region (join list items using a space)
+        Print(f"key={key}, value={value}")
         phones[key] = value
+        Print(f"Current state of dictionary:")
         Print(phones)
 except IOError as e:
     print("Reading data from file failed!")
@@ -44,5 +52,6 @@ sortedKeys.sort()
 for key in sortedKeys:
     print(key + ":" + phones[key])
 
-# 4. Search for a given value    
-print(phones['District of Columbia'])
+# 4. Search for a given value
+key = "District of Columbia"    
+print(f"code for {key} = {phones[key]}")
