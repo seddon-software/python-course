00b0b9334ae32d291c09c1296d4fa1f04db35b8d
seddon-software <seddon_software@btinternet.com>
2022-07-29 23:43:24 +0100

    renamed runners in Level 2:21

--- src/21 Command Line Parameters/01_args.py
+++ src/21 Command Line Parameters/01_args.py
@@ -2,7 +2,9 @@ import sys
 
 print("Command line parameters")
 
+print(__file__)
 # simple approach to accessing command line arguments
 for i, arg in enumerate(sys.argv):
     print(f"{i}: {arg}")
 print()
+
