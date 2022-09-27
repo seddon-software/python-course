b44a5c702babd5b3fcb8391de34e10df6e480233
seddon-software <seddon_software@btinternet.com>
2022-09-27 15:13:32 +0100

    preparing for Level 1 course Sep 2022

--- Level 1/01.leap_years.py
+++ Level 1/01.leap_years.py
@@ -1,9 +1,15 @@
 '''
-leap years are divisible by 4 or 400 but not if divisible by 100
+Tropical Year = 365.252422 days
+Gregorian calendar adds 1 day every 4 years
+                   adds 1 day every 400 years
+which equates to 365 + 1/4 + 1/400 = 365.2525 days
 
-2000        is a leap year      (5 * 400)
-2003        is NOT a leap year  (not divisible by 4)
-2024        is a leap year      (506 * 4)
-2100        is NOT a leap year  (divisible by 100)
+Thus we need leap years every 4th year and every 4th century
+
+Examples:
+    2000        is a leap year      (5 * 400)
+    2003        is NOT a leap year  (not divisible by 4)
+    2024        is a leap year      (506 * 4)
+    2100        is NOT a leap year  (divisible by 100)
 '''
-
+print("Days in Gregorian Year:", 365 + 1/4 + 1/400)
