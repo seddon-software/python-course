ecc3856cc0f816fd9ddac88d2d3d36e6bc5731b1
seddon-software <seddon_software@btinternet.com>
2022-12-05 16:34:24 +0000

    final preparations for Level 3 course Dec 2022

---
+++ src/07 Classes and Objects/Exercise.py
@@ -0,0 +1,7 @@
+'''
+Create a class that represents a bank account.  Add methods to allow a customer to deposit() and withdraw() 
+money and provide a method getBalance().  Write a test harness to check out your class.
+
+Add a method to allow transfer of funds between two different accounts and add code to the harness to verify
+that it works.
+'''
