f0ab10a70e34e9b60145fde483720954bc584f32
seddon-software <seddon_software@btinternet.com>
2022-12-19 22:09:12 +0000

    working on Virtual Environments

--- src/19 Testing/03 Nose/01.run_nose.py
+++ src/19 Testing/03 Nose/01.run_nose.py
@@ -1,8 +1,12 @@
-# nose will pick up tests defined as 
-#   functions
-#   methods of a class
-#   unit tests
-#  Note, tests must be in a file containe the name "test"
+'''
+Nose will run tests defined in other files.  All tests must be contained in files containing the name "test".
+Nose can run tests defined as 
+            functions
+            methods of a class
+            unit tests
+
+n.b. nose2 has now been released (2019)
+'''
 
 import nose
 nose.run()
