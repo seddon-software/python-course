ba2808a7a62119f40e99549e8a5630684984e5ff
seddon-software <seddon_software@btinternet.com>
2022-12-21 17:25:12 +0000

    working on Level 4 course

--- src/25 Type Hints/01.install_mypy.py
+++ src/25 Type Hints/01.install_mypy.py
@@ -1,14 +1,21 @@
 '''
+Static Type Checking
+====================
+
+Starting from version 3.5, support of optional static typing was added to Python. PEP 484 -- Type Hints was 
+approved and implemented. This PEP adds support of optional declaration of types for method and function 
+arguments and their return values. Later, PEP 526 -- Syntax for Variable Annotations was implemented in Python 3.6,
+PEP 526 added the possibility to specify a variable type. 
+
+Type annotations for variables and methods can be used by static code analyzers and IDEs. They do not impact 
+runtime performance (annotations are ignored at runtime).  For now (2022) mypy is the most popular static type 
+checker for Python. Static typing can be useful not only for big projects, but also for smaller scripts.
+
 Install Mypy
 ============
-Mypy is an static type checker for Python and will use type hints to validate programs.  Mypy is already available 
+Mypy is a static type checker for Python and will use type hints to validate programs.  Mypy is already available 
 at DLS provided you are using python 3.8 or later.  On other systems you can install with:
-            sudo apt install mypy
-
-Once Mypy is installed, you can install the Python interface to Mypy with this script.
+            python -m pip install mypy --user
 '''
 
-# import os
-
-# # this installs the python interface to mypy
-# os.system("python -m pip install mypy --user")
+pass
