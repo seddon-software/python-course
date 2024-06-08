a94aa5184a7f8486f9145d45ec1b6b6e6b96fb51
seddon-software <seddon_software@btinternet.com>
2024-06-08 21:34:15 +0100

    updated classes solution

--- src/07 Classes and Objects/Exercise.py
+++ src/07 Classes and Objects/Exercise.py
@@ -1,7 +1,11 @@
 '''
 Create a class that represents a bank account.  Add methods to allow a customer to deposit() and withdraw() 
-money and provide a method getBalance().  Write a test harness to check out your class.
+money and provide a method getBalance().
 
-Add a method to allow transfer of funds between two different accounts and add code to the harness to verify
-that it works.
+Add a static method to your class that allows transfer of funds between two different accounts.
+
+Finally define an overdraft limit for the accounts and raise an exception if an account attempts to go 
+overdrawn.
+
+Write simple tests to check out your class.
 '''
--- src/07 Classes and Objects/Solution.py
+++ src/07 Classes and Objects/Solution.py
@@ -6,31 +6,74 @@ deposit, withdraw followed by display
 
 3. Add overdraft to account and test
 
-4. Use proper testing
-
 '''
+
+class Overdrawn(Exception): pass
+class UnknownAccount(Exception): pass
+
 class BankAccount:
     accounts = {}
-    def __init__(self, name, openingBalance):
+    def transfer(*, toAccountName, fromAccountName, value):
+        toAccount = BankAccount.getAccount(toAccountName)
+        fromAccount = BankAccount.getAccount(fromAccountName)
+        fromAccount.balance -= value
+        if fromAccount.isOverdrawn():
+            fromAccount.balance += value
+            fromAccount.withdraw(value)      # just to generate error message
+        else:
+            toAccount.balance += value
+
+
+    def __init__(self, name, openingBalance, overdraftLimit=1000):
         self.name = name
         self.balance = openingBalance
+        self.overdraftLimit = overdraftLimit
         BankAccount.accounts[name] = self
     def deposit(self, value):
         self.balance += value
+    def isOverdrawn(self):
+        return self.balance < self.overdraftLimit
+
     def withdraw(self, value):
-        self.balance -= value
+        balance = self.balance
+        try:
+            self.balance -= value
+            if self.balance < self.overdraftLimit:
+                raise Overdrawn()
+        except Overdrawn as e:
+            print(f"{self.name}: can't withdraw {value} - would go overdrawn")
+            self.balance = balance
+            
     def printAllAccounts():
         for name,account in BankAccount.accounts.items():
             print(f"{name}: {account.balance}")
     def getAccount(name):
         return BankAccount.accounts[name]
 
+print("\n***** set up 3 sample accounts")
 johnsAccount = BankAccount("John", 10000)
 alisAccount = BankAccount("Ali", 25000)
-suesAccount = BankAccount("Sue", 31000)
-
+suesAccount = BankAccount("Sue", 840)
 BankAccount.printAllAccounts()
 
+print("\n***** deposit money in accounts")
+johnsAccount.deposit(1000)
+alisAccount.deposit(2000)
+suesAccount.deposit(150)
+BankAccount.printAllAccounts()
+
+print("\n***** withdraw money in accounts")
+johnsAccount.withdraw(6000)
+alisAccount.withdraw(5000)
+suesAccount.withdraw(15)
+BankAccount.printAllAccounts()
+
+print("\n***** attempt a transfer of funds")
+BankAccount.transfer(fromAccountName="John", toAccountName="Ali", value=1000)
+BankAccount.transfer(fromAccountName="Sue", toAccountName="John", value=1000)
+BankAccount.printAllAccounts()
+
+print("\n***** perform a deposit")
 accountName = input("who's account are you working with? ")
 theValue = int(input("how much do you want to deposit? "))
 theAccount = BankAccount.getAccount(accountName)
