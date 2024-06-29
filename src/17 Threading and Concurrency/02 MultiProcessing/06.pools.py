a0ca201a15eb5a899deef8dcfe377372a2d7a055
seddon-software <seddon_software@btinternet.com>
2024-06-29 19:48:41 +0100

    more multithreading

--- src/17 Threading and Concurrency/02 MultiProcessing/06.pools.py
+++ src/17 Threading and Concurrency/02 MultiProcessing/06.pools.py
@@ -1,38 +1,59 @@
 '''
 Pools
 =====
-The Pool class allows you to easily create a pool of processes to share the load on a complex computation. 
+The Pool/ThreadPool class allows you to easily create a pool of processes/threads to share the load on a complex computation. 
+Unfortunately, the overhead of the pool means it might be quicker to use a single process/thread.
 
-Notes:
-    1) This program does not run well in vscode; run it on the terminal
-    2) For lower values of N, the version that doesn't use pools can be faster
+Not: The map function has 2 parameters:
+    parameter-1:    the transform to apply
+    parameter-2:    the iterable to transform
+
+Thus all items in the iterable get transformed.
 '''
 
-from multiprocessing import Pool
+POOL_SIZE = 5
+NUMBER = 1
 
-import os
-import timeit
+from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+
+import os, timeit
 
 def SingleProcess():
-    results = list(map(f, range(N)))
-    #    print(f"single process: {results}")
+    results = map(f, range(N))
+    return sum(results)
+
+def PoolOfThreads():
+    # now use the map function provided by the multiprocessing module
+    # to perform the same operation, but split across a pool of POOL_SIZE threads
+    # this function aggregates the results from each child process
+    with ThreadPool(POOL_SIZE) as pool:
+        results = pool.map(f, range(N))
+    return sum(results)
 
 def PoolOfProcesses():
     # now use the map function provided by the multiprocessing module
-    # to perform the same operation, but split across a pool of 5 processes
+    # to perform the same operation, but split across a pool of POOL_SIZE processes
     # this function aggregates the results from each child process
-    with Pool(12) as p:
-        results = p.map(f, range(N))    
-    #    print(f"multiple processes: {results}")        
+    with Pool(POOL_SIZE) as pool:
+        results = pool.map(f, range(N))
+    return sum(results)
 
 def f(x):
     return x**2
 
-if __name__ == '__main__': 
-    N = 20000000
+N = 20000000
 
-    secs = timeit.timeit(setup="from __main__ import PoolOfProcesses", stmt="PoolOfProcesses()", number=1)
+if __name__ == '__main__': 
+    print(f"Processes:   {PoolOfProcesses()}")
+    print(f"Threads:     {PoolOfThreads()}")
+    print(f"Single:      {SingleProcess()}")
+
+    secs = timeit.timeit(setup="from __main__ import PoolOfProcesses", stmt="PoolOfProcesses()", number=NUMBER)
     print(f"Pool of Processes takes {secs:6.2f} secs")
 
-    secs = timeit.timeit(setup="from __main__ import SingleProcess", stmt="SingleProcess()", number=1)
-    print(f"Single Process    takes {secs:6.2f} secs")
+    secs = timeit.timeit(setup="from __main__ import PoolOfThreads", stmt="PoolOfThreads()", number=NUMBER)
+    print(f"Pool of Threads takes   {secs:6.2f} secs")
+
+    secs = timeit.timeit(setup="from __main__ import SingleProcess", stmt="SingleProcess()", number=NUMBER)
+    print(f"Single Process takes    {secs:6.2f} secs")
