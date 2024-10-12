9408b1cdd2c5a1ef8552a7efe597f7ef427b383f
seddon-software <seddon_software@btinternet.com>
2024-10-12 21:59:12 +0100

    got C_Library working wih setup.py

--- src/16 Scientific Libraries/02 Numpy/C_Library/build_install_and_test.py
+++ src/16 Scientific Libraries/02 Numpy/C_Library/build_install_and_test.py
@@ -28,23 +28,24 @@ call("rm -rf build")
 printMessage("uninstall previous version of extension module (if it exists)")
 call("python -m pip uninstall --yes roots")
 
-# install new version
-printMessage("install new version")
-call("python -m pip install -e .")
+# # install new version
+# printMessage("install new version")
+# call("python -m pip install --force-reinstall -e .")
 
-# clean up
-printMessage("clean up")
-call("rm -rf build")
-call("rm -rf roots.egg-info")
-soFile = glob.glob('*.so')[0]
-call(f"rm -rf {soFile}")
-call("tree .")
+# # clean up
+# printMessage("clean up")
+# call("rm -rf build")
+# call("rm -rf roots.egg-info")
+# soFile = glob.glob('*.so')[0]
+# call(f"rm -rf {soFile}")
+# call("tree .")
+# os.system("pip freeze | grep roots")
 
-# test in new folder
-printMessage("test in new folder")
-os.chdir("TestFolder")
-call("python testit.py")
-os.chdir("..")
+# # test in new folder
+# printMessage("test in new folder")
+# os.chdir("TestFolder")
+# call("python testit.py")
+# os.chdir("..")
 
 # build wheel
 printMessage("build wheel")
@@ -53,20 +54,20 @@ call("python -m pip wheel -e .")
 # install wheel
 printMessage("install wheel")
 wheelFile = glob.glob("*.whl")[0]
-call(f"python -m pip install {wheelFile}")
+call(f"python -m pip install --force-reinstall {wheelFile}")
 
-# test again
-printMessage("test again")
+# test in new folder
+printMessage("test in new folder")
 os.chdir("TestFolder")
 call("python testit.py")
 os.chdir("..")
 
-# # clean up
-# printMessage("clean up")
+# clean up
+printMessage("clean up")
 # call("rm -rf dist")
-# call("rm -rf build")
-# call(f"rm {wheelFile}")
+call("rm -rf build")
+call(f"rm {wheelFile}")
 # eggFolder = glob.glob("*.egg-info")[0]
-# call("rm -rf roots.egg-info")
-# call("tree .")
+call("rm -rf roots.egg-info")
+call("tree .")
 
