3f1310cf97a4a3840ff49814db81eda289e46ff2
Chris <seddon_software@btinternet.com>
2025-02-18 15:45:59 +0000

    minor stuff

---
+++ src/47 Unicode/01.useful_websites.py
@@ -0,0 +1,16 @@
+'''
+Useful Websites
+===============
+The first site has a list of Ascii characters and the other sites have lists of Unicode characters.
+'''
+
+import webbrowser
+
+# ascii characters
+webbrowser.open("https://www.asciitable.com/")
+
+# full list of Unicode characters
+webbrowser.open("https://symbl.cc/en/unicode-table/")
+
+# common unicode mathematical symbols
+webbrowser.open("https://math.typeit.org/")
