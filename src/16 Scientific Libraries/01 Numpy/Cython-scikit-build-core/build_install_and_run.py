a33e0ca2e4630327f8b8f833f4658ab820a286d6
Chris <seddon_software@btinternet.com>
2025-04-10 21:28:29 +0100

    tidied cython example

---
+++ src/16 Scientific Libraries/01 Numpy/Cython-scikit-build-core/build_install_and_run.py
@@ -0,0 +1,52 @@
+'''
+Run this script from the command line (difficult to read output in VSCode)
+'''
+
+import os, sys, subprocess, time, glob
+
+N = 0
+MODULE = "cythonRoots"
+
+def printMessage(m):
+    time.sleep(N)
+    print()
+    print(m)
+    print("-" *  len(m))
+    print()
+    input("?")
+
+def call(cmd):
+    print(cmd)
+    time.sleep(N)
+    result = subprocess.run(cmd.split())
+    try:
+        result.check_returncode()
+    except Exception as e:
+        print(e)
+        print(if_build_failed)
+
+
+call ("clear")    
+
+if_build_failed = """if the build fails because of 'failed to map segment from shared object'
+you need to run: 
+\texport TERM=~/tmp"""
+printMessage(message)
+printMessage("build with pipx")
+call("python -m pipx run build")
+
+printMessage("install with pip")
+os.system("python -m pip install .")
+
+printMessage(f"test module: {MODULE}")
+
+import cythonRoots as roots
+print(f"result = {roots.sumOfRoots(10)}")
+
+printMessage("pip show")
+call("python -m pip show cythonRoots")
+printMessage("spec of module")
+print(roots.__spec__)
+
+printMessage("clean up")
+call("rm -rf dist")
