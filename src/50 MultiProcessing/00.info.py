9ca62f74cc5c85f645951e20e5b1ffd9b7a19448
Chris <seddon_software@btinternet.com>
2025-04-12 19:57:39 +0100

    minor

---
+++ src/50 MultiProcessing/00.info.py
@@ -0,0 +1,11 @@
+'''
+A regular Python program will execute on a single CPU.
+
+If you wish to use more than one CPU (to fully utilize your system), there are three ways 
+you can achieve this by utilising any of the following modules:
+    multiprocessing.Process
+    multiprocessing.Pool
+    concurrent.futures.ProcessPoolExecutor
+'''
+
+pass
