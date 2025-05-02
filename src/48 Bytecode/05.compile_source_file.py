73d350d3f0a1b701d1ac64d54606e494ac5fc38e
Eu-Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-05-02 12:50:38 +0100

    Tidied up whitespace for files in 'src/48 Bytecode'

--- src/48 Bytecode/05.compile_source_file.py
+++ src/48 Bytecode/05.compile_source_file.py
@@ -3,7 +3,7 @@ The Bytecode Cache
 ==================
 
 The py-compile module allows us to compile source code and produce a bytecode file.  The resulting file will have
-a .pyc extension.  This is the file you will sometimes find in the __pycache__ folder.  It is never necessary to 
+a .pyc extension.  This is the file you will sometimes find in the __pycache__ folder.  It is never necessary to
 compile source code ourselves; CPython will compile code automatically to refresh the cache.  Indeed, We are only
 compiling this code to show what gets put in the cache.
 '''
@@ -15,4 +15,3 @@ py_compile.compile("myfile.py")
 os.system("hexdump __pycache__/*.pyc")
 os.system("file __pycache__/*.pyc")
 os.system("ls -l __pycache__")
-
