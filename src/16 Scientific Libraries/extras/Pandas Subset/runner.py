b298239c4245bace03ec6268fbe91b4381c01734
Chris <seddon_software@btinternet.com>
2025-08-30 10:49:40 +0100

    minor changes

--- src/16 Scientific Libraries/extras/Pandas Subset/runner.py
+++ src/16 Scientific Libraries/extras/Pandas Subset/runner.py
@@ -1,5 +1,8 @@
 '''
 Fire up Jupyter notebook and look at the topics of types and mutability.
+You may need to install first:
+    pip install jupyter notebook
+    pip install jupyterlab
 '''
 
 import os
