26ef24acdb98be486bb20c10351027b9cd61409b
Chris <seddon_software@btinternet.com>
2025-09-09 16:31:44 +0100

    minor stuff

--- CODE.sh
+++ CODE.sh
@@ -1,3 +1,6 @@
+# run this script to install python in VSCode:
+#       chmod +x ./CODE.sh
+#       ./CODE.sh
 code --install-extension ms-python.python
 code --install-extension ms-python.debugpy
 code .
