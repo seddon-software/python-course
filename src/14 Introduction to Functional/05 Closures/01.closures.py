92ecc8cd410242f5d9f5245023a71318d9ca026a
Chris <seddon_software@btinternet.com>
2025-09-20 23:52:09 +0100

    minor stuff

--- src/14 Introduction to Functional/05 Closures/01.closures.py
+++ src/14 Introduction to Functional/05 Closures/01.closures.py
@@ -1,7 +1,6 @@
 '''
 Closures
 ========
-
 Python allows nested functions as the example below.  Note that when calling the nested function "g" we don't
 pass the x and y lists to "g" as parameters even though we modify x and y in "g".  This is because of closures.
 
@@ -9,8 +8,12 @@ The function "g" sees variables defined in its environment (or closure).  The en
 surrounding scope of "g".  Python allows us to explicity see the closure with (see utils.py)
             <function>.__closure__
 
+A closure is when an inner function retains access to its outer function's variables, even after the outer 
+function has finished executing. It "remembers" the environment in which it was created, allowing it to access 
+variables outside its immediate scope.
+
 In this example, the closure has 2 items (x and y).  Note that only mutable items referenced in "g" will be
-included in this closure (so a is not part of the closure).
+included in this closure (so variable a is not part of the closure).
 '''
 
 from utils import *     # for displayClosures
