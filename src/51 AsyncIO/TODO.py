3b3e6a34e159b1449185fea8ac87631220fbf7d7
Chris <seddon_software@btinternet.com>
2026-05-05 20:53:47 +0100

    minor

--- src/51 AsyncIO/TODO.py
+++
@@ -1,8 +0,0 @@
-do an example with sync threads and sync processes
-must use if __name__ == "__main__"
-
-https://www.youtube.com/watch?v=oAkLSJNr5zY
-https://github.com/CoreyMSchafer/AsyncIO-Code-Examples
-example showing hundreds of coroutines running
-
-aiofiles
